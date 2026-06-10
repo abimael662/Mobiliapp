f962ab2e5435c8b02c312093dfbfa6cce9384e71
abimael662 <abimaelmartinez245@gmail.com>
2026-06-09 20:21:31 -0600

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'mobili-app',
+  webDir: 'dist'
+};
+
+export default config;
