8266468672e7a054d4752f72f3e085b1ed2b380a
Abimael Martinez <abimaelmartinez245@gmail.com>
2026-07-22 15:34:32 -0600

    feat(notification): integrate reusable components

--- frontend/src/presentation/views/notifications/NotificationPage.vue
+++ frontend/src/presentation/views/notifications/NotificationPage.vue
@@ -1,18 +1,25 @@
 <template>
   <ion-page>
-    <ion-header>
+    <ion-header class="ion-no-border">
       <ion-toolbar>
         <ion-title>Notificaciones</ion-title>
       </ion-toolbar>
     </ion-header>
 
     <ion-content class="ion-padding">
-      <ion-card>
-        <ion-card-content> Notificaciones </ion-card-content>
-      </ion-card>
+      
+      <CardInfo
+        imagen="https://docs-demo.ionic.io/assets/madison.jpg"
+        descripcion="Sala creada"
+        titulo="Nombre Notificacion"
+        sala="Info..."
+        :hora="2"
+        enlace="/tabs/rooms/:id"
+      />
+
     </ion-content>
 
-    <ion-footer>
+    <ion-footer class="ion-no-border">
       <ion-toolbar class="ion-padding-horizontal">
         <ion-button expand="block" fill="outline">
           Marcar todas como leídas
@@ -34,4 +41,6 @@ import {
   IonFooter,
   IonButton,
 } from "@ionic/vue";
+
+import CardInfo from "@/presentation/components/widget/CardInfo.vue";
 </script>
