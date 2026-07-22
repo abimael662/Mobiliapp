6807eea91bad6878844a99b203aff6ddf277acd0
Abimael Martinez <abimaelmartinez245@gmail.com>
2026-07-22 16:15:36 -0600

    refactor(components): update reusable UI component

--- frontend/src/presentation/components/widget/CardButton.vue
+++ frontend/src/presentation/components/widget/CardButton.vue
@@ -1,17 +1,18 @@
 <template>
   <ion-col size="6">
-    <ion-card button :router-link="enlace">
-      <ion-card-content class="ion-justify-content-center ion-text-center ion-align-items-center ion-flex-column ion-display-flex">
+    <ion-card button :router-link="enlace" class="custom-card">
+      <ion-card-content class="card-content">
         <span class="material-symbols-outlined card-icon">
           {{ icono }}
         </span>
+
         <h2>{{ titulo }}</h2>
       </ion-card-content>
     </ion-card>
   </ion-col>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { IonCol, IonCard, IonCardContent } from '@ionic/vue';
 
 defineProps({
@@ -22,8 +23,27 @@ defineProps({
 </script>
 
 <style scoped>
+.custom-card {
+  height: 140px;
+  margin: 8px;
+}
+
+.card-content {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
 .card-icon {
   font-size: 48px;
   margin-bottom: 8px;
+}
+
+h2 {
+  font-size: 16px;
+  text-align: center;
+  margin: 0;
 }
 </style>
